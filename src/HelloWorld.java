eef27a675a935bafff82c810756cd646ea1aa528
Study1278 <singhmayank79820@gmail.com>
2026-06-24 12:26:45 +0530

    init: hello world - first java program

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+// Java Hello World! Program
+public class HelloWorld{
+    /* This is the main function.
+    this is where the execution of
+    a java program begins */
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
