e4adac7ff829d920e8a686318b0fdad8b0adebce
Study1278 <singhmayank79820@gmail.com>
2026-09-19 09:34:54 +0530

    comment file

---
+++ src/comment.java
@@ -0,0 +1,78 @@
+public class comment {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
+
+
+//thats the Swapping of two Numbers
+/*public class Main{
+    public static void main(String[] args){
+        int a = 8;
+        int b= 10;
+        int temp = 0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
+*/
+/*
+class point{
+    int x ;
+    int y;
+}
+class test{
+    public static void main(String[] args){
+        point p = new point();
+        p.x =10;
+        p.y=8;
+        System.out.println(p.x + " " + p.y);
+    }
+}
+*/
+/*
+class test{
+    int x;
+    int y;
+}
+class point{
+    public static void main(String[] args){
+        test t1 = new test();
+        t1.x= 52;
+        test t2 = t1 ;
+        t2.x = 68;
+        System.out.println(t1.x);
+        System.out.println(t2.x);
+    }
+
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
